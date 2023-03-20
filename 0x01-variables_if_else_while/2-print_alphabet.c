0f123a38c5dd8533ad7ceb1d0364e23975765d45
engineerck <charleskalomo@gmail.com>
2023-03-20 10:47:32 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - main function 
+ *
+ * Return always 0
+ * */
+
 int main(void)
+	
 {
-char c = 'a';
-while (c <= 'z')
-{
-putchar(c);
-c++;
-}
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
 putchar('\n');
 return (0);
 }

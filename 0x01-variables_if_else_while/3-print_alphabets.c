1b24797b35e7cf0c51c8c48c4bfe7d15ce20e6b4
engineerck <charleskalomo@gmail.com>
2023-03-20 04:32:03 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,19 +5,11 @@
  *
  * Return: always 0
  */
-
 int main(void)
 {
-char lowercase = 'a';
-char uppercase = 'A';
-while (lowercase <= 'z')
-{
-putchar(lowercase++);
-}
-while (uppercase <= 'Z')
-{
-putchar(uppercase++);
-}
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
 putchar('\n');
-return 0;
+return (0);
 }

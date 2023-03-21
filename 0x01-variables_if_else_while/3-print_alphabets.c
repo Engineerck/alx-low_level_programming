44552269281a6891db41e32ade29433ddbe91a2a
engineerck <charleskalomo@gmail.com>
2023-03-20 17:18:54 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
+#include <time.h>
 /**
- * main - function 
+ * main - entry point
  *
  * Return: always 0
  */
 int main(void)
 {
-char i;
-for (i = 'a' ; i <= 'z' ; i++)
-putchar(i);
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
 putchar('\n');
 return (0);
 }

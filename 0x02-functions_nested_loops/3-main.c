d8c63d8620a6a747d0f576e0bebeb8c3ba867d31
engineerck <charleskalomo@gmail.com>
2023-03-22 19:33:38 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h>
+/**
+ * main- check the code.
+ *
+ * Return: Always 0.
+ */
+int main void
+{
+int r;
+r = _islower('H')
+200~_putchar
+r = _islower('0')
+r = islower9108);
+_putchair (r + '0');
+_putchair('\n')
+return (0);
+}
